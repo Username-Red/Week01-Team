592841a9ed7da6503ce08d633a1c7f08d1ce9d78
benarivo66 <iz4comed@yahoo.com>
2024-11-19 13:26:26 +0100

    alerts

--- src/js/cart.js
+++ src/js/cart.js
@@ -14,7 +14,6 @@ function renderCartContents() {
     // Calculate the total cost of the items
     const total = cartItems.reduce((sum, item) => sum + item.FinalPrice, 0);
 
-
     // Show the cart footer and update the total
     cartFooter.classList.remove("hide");
     cartTotal.textContent = `Total: $${total.toFixed(2)}`;
@@ -35,7 +34,7 @@ function cartItemTemplate(item) {
     </a>
     <p class="cart-card__color">${item.Colors[0].ColorName}</p>
     <p class="cart-card__quantity">qty: 1</p>
-    <p class="cart-card__price">$${item.FinalPrice} <br> ${((item.SuggestedRetailPrice - item.FinalPrice) / item.SuggestedRetailPrice * 100).toFixed(0)}% Off</p>
+    <p class="cart-card__price">$${item.FinalPrice} <br> ${(((item.SuggestedRetailPrice - item.FinalPrice) / item.SuggestedRetailPrice) * 100).toFixed(0)}% Off</p>
   </li>`;
 }
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 import ProductData from "./ProductData.mjs";
 import ProductListing from "./ProductList.mjs";
 import { qs } from "./utils.mjs";
+import Alert from "./Alert.js";
 
+const alert = new Alert("alerts");
+alert.renderElement();
 const main = async () => {
   const dataSource = new ProductData("tents");
   const listElement = qs(".product-list");
